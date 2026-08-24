71a8a81ec392a3ba4851838ab763b17f10f6eaa2
Amit Hinduja <amit@satgurusolutions.com>
2026-08-24 18:47:19 +0700

    Minor fixes

--- components/home/home/index.tsx
+++ components/home/home/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       {/*<InfiniteTicker />*/}
       <CategoryOptions />
       <Designer />
-      <Projects />
+      {/*<Projects />*/}
       <Finance />
       <ConcreteSection />
       <Review />
--- components/home/page.tsx
+++ components/home/page.tsx
@@ -28,9 +28,9 @@ export default function HomePage() {
         <Designer />
       </section>
 
-      <section className="py-20 lg:py-28">
+      {/*<section className="py-20 lg:py-28">
         <Projects />
-      </section>
+      </section>*/}
 
       <section className="py-20 lg:py-28">
         <Finance />
